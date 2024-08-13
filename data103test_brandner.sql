40de6ae93ec9c86c11aa983aefa49376ad7b371a
panupong-d <panupong-d@rmutp.ac.th>
2024-08-13 11:37:50 +0700

    first commit

---
+++ data103test_brandner.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.data103test (image) VALUES
+	 ('https://lh3.googleusercontent.com/d/1lwPR-m87qqTxAFZvpZYh0yAnglTLMEU7'),
+	 ('https://lh3.googleusercontent.com/d/1lwPR-m87qqTxAFZvpZYh0yAnglTLMEU7'),
+	 ('https://lh3.googleusercontent.com/d/1lwPR-m87qqTxAFZvpZYh0yAnglTLMEU7');
